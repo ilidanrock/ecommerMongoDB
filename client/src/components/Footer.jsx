aa9ab1d7c79267a2b7b619fc00407647e7017921
Luis <rluis747@gmail.com>
2022-04-22 19:55:13 -0500

    Ready footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import styled from "styled-components";
+import AddLocationAltIcon from "@mui/icons-material/AddLocationAlt";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import EmailIcon from "@mui/icons-material/Email";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+const Logo = styled.h1``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const ContacItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
+`
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>LLAMA</Logo>
+        <Desc>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularized in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <FacebookIcon />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <InstagramIcon />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <TwitterIcon />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <PinterestIcon />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContacItem>
+          <AddLocationAltIcon style={{marginRight: '10px' }} />
+          Jr Tapacocha 4961, Los Olivos, Lima,Peru .
+        </ContacItem>
+        <ContacItem>
+          <LocalPhoneIcon style={{marginRight: '10px' }} />
+          +51920300340
+        </ContacItem>
+        <ContacItem>
+          <EmailIcon style={{marginRight: '10px' }} />
+          rluis747@gmail.com
+        </ContacItem>
+        <Payment src='https://i.ibb.co/Qfvn4z6/payment.png' />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
