da6ccc666d8ed0d72a276c7b09eac3c88da2b0ed
Luis <rluis747@gmail.com>
2022-04-24 20:19:44 -0500

    Ready page productdetail

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -3,46 +3,158 @@ import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
-const Container = styled.div`
-    
-`
+const Container = styled.div``;
 const Wrapper = styled.div`
-    padding: 50px;
-    display: flex;
-`
+  padding: 50px;
+  display: flex;
+`;
 const ImageContainer = styled.div`
-    flex: 1;
-`
+  flex: 1;
+`;
 const Image = styled.img`
-    width:100%;
-    height: 90vh;
-`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
 const InfoContainer = styled.div`
-    flex: 1;
+  flex: 1;
+  padding: 0px 50px;
+`;
+const Title = styled.h1`
+  font-weight: 200;
+`;
+const Des = styled.p`
+  margin: 20px 0px;
+`;
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
 `
-const Title = styled.h1``
-const Des = styled.p``
-const Price =styled.span``
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+    margin: 0 5px;
+    cursor: pointer;
+`
+const FilterSize = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+`
+const FilterSizeOption = styled.option``
+
+const AddContainer = styled.div`
+width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+`
+
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+
+`
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`
+
+const Button =styled.button`
+    padding: 15px;
+    border: 1px solid teal;
+    background-color: white;
+    cursor: pointer;
+    font-weight: 500;
+
+    &:hover{
+        background-color: #f8f4f4;
+    }
+`
 
 const Product = () => {
   return (
     <Container>
-        <Navbar/>
-        <Announcement/>
-        <Wrapper>
-            <ImageContainer>
-                <Image src='https://img.abercrombie.com/is/image/anf/KIC_155-1116-2660-278_prod1?policy=product-medium&wid=350&hei=438' />
-            </ImageContainer>
-            <InfoContainer>
-                <Title>Jacket</Title>
-                <Des></Des>
-                <Price> $ 1000000 </Price>
-            </InfoContainer>
-        </Wrapper>
-        <Footer/>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImageContainer>
+          <Image src="https://img.abercrombie.com/is/image/anf/KIC_155-1116-2660-278_prod1?policy=product-medium&wid=350&hei=438" />
+        </ImageContainer>
+        <InfoContainer>
+          <Title>Jacket</Title>
+          <Des>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like).
+          </Des>
+          <Price>$ 1000000 </Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+                <FilterTitle>Size</FilterTitle>
+                <FilterSize>
+                    <FilterSizeOption>XS</FilterSizeOption>
+                    <FilterSizeOption>S</FilterSizeOption>
+                    <FilterSizeOption>M</FilterSizeOption>
+                    <FilterSizeOption>L</FilterSizeOption>
+                    <FilterSizeOption>XL</FilterSizeOption>
+                </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+              <AmountContainer>
+                  <RemoveCircleIcon/>
+                  <Amount>1</Amount>
+                  <AddCircleIcon/>
+              </AmountContainer>
+              <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Footer />
     </Container>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
