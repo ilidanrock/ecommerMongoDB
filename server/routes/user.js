73abf4d76ddf9299de8fb34de6f69c610a6af9a6
Luis <rluis747@gmail.com>
2022-06-06 18:10:54 -0500

    Ready user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -83,4 +83,35 @@ router.get(
     }
   }
 );
+
+//GET USER STATS
+
+router.get(
+  "/stats",
+  passport.authenticate("jwt", { session: false }),
+  verifyTokenAndAdmin,
+  async (req, res, next) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+    try {
+      const data = await User.aggregate([
+        { $match: { createdAt: { $gte: lastYear } } },
+        {
+          $project: {
+            month: { $month: "$createdAt" },
+          },
+        },
+        {
+          $group: {
+            _id: "$month",
+            total: { $sum: 1 },
+          },
+        },
+      ]);
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+);
 module.exports = router;
